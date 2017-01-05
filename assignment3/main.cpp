b209e9e0a522d32906180a7ccce7e3cd1a30de26
dawnfan <fanjiadawn@gmail.com>
2017-01-05 15:48:25 +0800

    update

---
+++ assignment3/main.cpp
@@ -0,0 +1,28 @@
+#include "StereoMatching.h"
+
+int main()
+{
+	double** LeftImageMat;
+	double** RightImageMat;
+	//LeftImageMat = NULL;
+	//RightImageMat = NULL;
+	ReadImageTxt("L.txt", LeftImageMat);
+	ReadImageTxt("R.txt", RightImageMat);
+
+	MatchStereo(LeftImageMat, RightImageMat);
+	WriteTxtImage("out.txt", OutImageMat);
+
+
+	// delete the mat data
+	for (int t_i = 0; t_i < ImageRawNum; t_i++)
+	{
+		delete[]RightImageMat[t_i];
+		delete[]LeftImageMat[t_i];
+		delete[]OutImageMat[t_i];
+	}
+	delete[]RightImageMat;
+	delete[]LeftImageMat;
+	delete[]OutImageMat;
+
+	return 0;
+}
